2c157890b65333b57bc15ba6ca2b30c733013fb5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-05 19:10:40 +0000

    Initial Commit

---
+++ src/main/java/com/example/crud_springboot_react_javaguides/CrudSpringbootReactJavaguidesApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_springboot_react_javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootReactJavaguidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootReactJavaguidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_springboot_react_javaguides/CrudSpringbootReactJavaguidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_springboot_react_javaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootReactJavaguidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
