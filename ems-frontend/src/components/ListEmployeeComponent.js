70385bc019fee5d9726487290dcef4741b202fe0
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-06 22:33:59 +0000

    Add delete functionality to ListEmployeeComponent

--- ems-frontend/src/components/ListEmployeeComponent.js
+++ ems-frontend/src/components/ListEmployeeComponent.js
@@ -1,54 +1,70 @@
 import React, { useState, useEffect } from "react";
 
-import { listEmployees } from "../service/EmployeeService";
+import { deleteEmployee, listEmployees } from "../service/EmployeeService";
 
 const ListEmployeeComponent = () => {
-    const [employees, setEmployees] = useState([]);
+  const [employees, setEmployees] = useState([]);
 
-    useEffect(() => {
-        getAllEmployees()
-    }, [])
+  useEffect(() => {
+    getAllEmployees();
+  }, []);
 
-    const getAllEmployees = () => {
-        listEmployees()
-            .then((response) => {
-            setEmployees(response.data);
-            console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
+  const getAllEmployees = () => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <div className="container">
-            <h2 className="text-center"> List Employees </h2>
-            <table className="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th> Employee ID </th>
-                        <th> Employee First Name </th>
-                        <th> Emplyee Last Name </th>
-                        <th> Employee Email ID </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        employees.map(
-                            employee => 
-                                <tr key={employee.id}>
-                                    <td>{employee.id}</td>
-                                    <td>{employee.firstName}</td>
-                                    <td>{employee.lastName}</td>
-                                    <td>{employee.email}</td>
-                                </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+  const removeEmployee = (employeeId) => {
+    deleteEmployee(employeeId)
+      .then((response) => {
+        getAllEmployees();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  return (
+    <div className="container">
+      <h2 className="text-center"> List Employees </h2>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th> Employee ID </th>
+            <th> Employee First Name </th>
+            <th> Emplyee Last Name </th>
+            <th> Employee Email ID </th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+              <td>
+                {" "}
+                <button
+                  className="btn tbn-danger"
+                  onClick={() => removeEmployee(employee.id)}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ListEmployeeComponent;
