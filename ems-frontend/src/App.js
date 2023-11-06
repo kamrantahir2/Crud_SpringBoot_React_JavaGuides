5ce0f2556d8996bb20c232ee5ae56d3640327016
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-06 20:47:49 +0000

    Install react-router-dom, Implement in App.js

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={<ListEmployeeComponent />}></Route>
+            <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+          </Routes>
+        </div>
+        <FooterComponent />
+      </BrowserRouter>
+
     </div>
   );
 }
